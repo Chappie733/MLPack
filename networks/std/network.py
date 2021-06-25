b9fde42058c70d45120f6a6feeee41a2d60bf2c9
Chappie733 <41633580+Chappie733@users.noreply.github.com>
2021-06-25 19:39:23 +0200

    added support for optimizers

--- networks/std/network.py
+++ networks/std/network.py
@@ -1,17 +1,17 @@
 import numpy as np
-from networks.std.layer import Layer, ELU
+from networks.std.layer import Layer
 
 class Model:
 
-	def __init__(self, layers, lr=0.001):
+	def __init__(self, layers):
 		self.L = len(layers)
-		self.lr = lr
 		self.layers = layers
 
-	def compile(self, error_func):
+	def compile(self, error_func, optimizer):
 		for l in range(self.L-1):
 			self.layers[l].log_next(self.layers[l+1].N)
 		self.error_func = error_func
+		self.optimizer = optimizer
 
 	def _predict(self, x):
 		self.layers[0].feed(x)
@@ -33,6 +33,8 @@ class Model:
 		return self.error_func(Y, predictions)
 
 	def fit(self, inputs, labels, epochs=75, verbose=True):
+		epochs_errors = []
+
 		for epoch in range(1, epochs+1):
 			if verbose:
 				H = 0
@@ -56,10 +58,17 @@ class Model:
 					errors.append(error_l*g_prime)
 
 				for l in range(1, self.L):
+					weights_grads = np.zeros(self.layers[l-1].weights.shape)
+					thresholds_grads = errors[-l]
 					for j in range(self.layers[l].N):
 						for i in range(self.layers[l-1].N):
-							self.layers[l-1].weights[j][i] -= self.lr*errors[-l][j]*self.layers[l-1].neurons[i]
-					self.layers[l].thresholds -= self.lr*errors[-l]
+							weights_grads[j][i] = errors[-l][j]*self.layers[l-1].neurons[i]
+				
+					gradients = np.append(weights_grads, thresholds_grads)
+					updates = self.optimizer.step(gradients, layer=l-1)
+					self.layers[l-1].weights += np.reshape(updates[:-self.layers[l].N], (self.layers[l].N, self.layers[l-1].N))
+					self.layers[l].thresholds += updates[-self.layers[l].N:]
 
-
-			print("Epoch #{epoch}: {H}".format(epoch=epoch, H=H))
+			print("Error on epoch #{epoch}: {H}".format(epoch=epoch, H=H))
+			epochs_errors.append(H)
+		return epochs_errors
