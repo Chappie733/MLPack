55c0cb3aac34726ff1947617acae31117743e7b1
Chappie733 <41633580+Chappie733@users.noreply.github.com>
2021-07-11 23:19:25 +0200

    bug fix

--- networks/std/network.py
+++ networks/std/network.py
@@ -1,10 +1,13 @@
 import numpy as np
+import os
+import h5py
 
 class Model:
 
-	def __init__(self, layers):
+	def __init__(self, layers, name='model'):
 		self.L = len(layers)
 		self.layers = layers
+		self.name = name
 
 	def compile(self, error_func, optimizer):
 		for l in range(self.L-1):
@@ -31,8 +34,7 @@ class Model:
 			Y = np.array(Y)
 		predictions = self.predict(X)
 		return self.error_func(Y, predictions)
-	
-	# trains the network with stochastic gradient descent
+
 	def fit_stochastic(self, inputs, labels, epochs=75, verbose=True, return_errors=False):
 		if return_errors:
 			vals = []
@@ -43,13 +45,13 @@ class Model:
 
 			for u in range(len(inputs)):
 				X, Y = inputs[u], labels[u]
+				predicted = self._predict(X)
 
 				if verbose or return_errors:
-					predicted = self._predict(X)
 					H += self.error_func(Y, predicted)
 
 				B_L = self.layers[-2].get_local_fields(self.layers[-1].thresholds)
-				errors_L = self.error_func.grad(Y, predicted)*self.layers[-2].g(B_L, deriv=True)
+				errors_L = self.error_func.grad(Y, predicted)*self.layers[-1].g(B_L, deriv=True)
 				errors = [errors_L]
 
 				for l in range(self.L-1, 1, -1):
@@ -94,13 +96,13 @@ class Model:
 				updates = [np.zeros(shape=(self.layers[i].N*(self.layers[i-1].N+1),)) for i in range(1,self.L)]
 				for u in range(batch*batch_size, min((batch+1)*batch_size, len(inputs))):
 					X, Y = inputs[u], labels[u]
-
+					predicted = self._predict(X)
+					
 					if verbose or return_errors:
-						predicted = self._predict(X)
 						H += self.error_func(Y, predicted)
 
 					B_L = self.layers[-2].get_local_fields(self.layers[-1].thresholds)
-					errors_L = self.error_func.grad(Y, predicted)*self.layers[-2].g(B_L, deriv=True)
+					errors_L = self.error_func.grad(Y, predicted)*self.layers[-1].g(B_L, deriv=True)
 					errors = [errors_L]
 
 					for l in range(self.L-1, 1, -1):
@@ -129,4 +131,26 @@ class Model:
 			if return_errors:
 				vals.append(H)
 		if return_errors:
-			return vals
+			return vals	
+
+	def __str__(self):
+		res = f"Sequential Model: name: {self.name}, layers: {self.L}\n"
+		trainable_params = 0
+		for layer in self.layers:
+			res += str(layer)
+			trainable_params += layer.get_trainable_params()
+		res += f"Trainable parameters: {trainable_params}"
+		return res
+
+	def save(self, filename, absolute=False):
+		dirname = os.getcwd()
+		path = filename if absolute else os.path.join(dirname, filename)
+		file = h5py.File(path, 'w')
+		structure = np.array([[layer.N, layer._type] for layer in self.layers], dtype=np.uint32)
+		for i in range(self.L):
+			self.layers[i].save(file, i)
+
+		file.create_dataset('structure', structure.shape, np.uint32, structure, compression="gzip")
+		name = np.array([ord(x) for x in self.name], dtype=np.ubyte)
+		file.create_dataset('name', name.shape, np.ubyte, name, compression='gzip')
+		file.close()
